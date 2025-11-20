76a4cd15b56c205cba1cb541a76b2d99aa499df7
ranggo pato <patoranggo@gmail.com>
2025-11-20 19:10:21 +0700

    materi: interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-  console.log('Hi, Ranggo!')
+  console.log('Hi, Ranggo!');
+  const nama = 'Pato';
 </script>
 
 <template>
   <div class="container">
-    <h1>Hello, gua Ranggo</h1>
+    <h1>Hello, gua {{ nama }}</h1>
     <p>
       Gua sebagai tutor vue pada sesi kali ini
     </p>   
