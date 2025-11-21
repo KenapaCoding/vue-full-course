47ddefa09597dc9dddc0d59203727bf8d44f4c50
ranggo pato <patoranggo@gmail.com>
2025-11-21 09:44:58 +0700

    materi: methods dan event listener

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,30 @@
   import { ref, reactive } from 'vue';
 
   const counter = reactive({count:0})
-  counter.count++
-
+  function increment(number){
+    counter.count = counter.count + number
+  }
+  
   const message = ref("Hi")
   message.value = "Hello"
 
   console.log('Hi, Ranggo!');
-  const nama = 'Pato';
+  function greeting(nama){
+    return `Hello, gua ${nama}`
+  }
 
   const link = "https://vuejs.org";
 </script>
 
 <template>
   <div class="container">
-    <h1>Hello, gua {{ nama }}</h1>
+    <h1>{{ greeting('Ranggo') }}</h1>
     <p>
       Gua sebagai tutor vue pada sesi kali ini
     </p>   
   </div>
   <div class="container">
-    <p>{{ counter.count }}</p>
+    <button @click="increment(2)"> Count is : {{ counter.count }}</button>
     <p>{{ message }}</p>
     <p>{{ message.split('').reverse().join('') }}</p>
     <a :href="link">Kunjungi Vue</a>
