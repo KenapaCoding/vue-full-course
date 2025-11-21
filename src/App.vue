903d1ee3efd1228223d97afed4b702887dee93d0
ranggo pato <patoranggo@gmail.com>
2025-11-21 08:27:05 +0700

    materi: deklaratif rendering

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <script setup>
+  import { ref, reactive } from 'vue';
+
+  const counter = reactive({count:0})
+  counter.count++
+
+  const message = ref("Hi")
+  message.value = "Hello"
+
   console.log('Hi, Ranggo!');
   const nama = 'Pato';
 </script>
@@ -9,6 +17,11 @@
     <p>
       Gua sebagai tutor vue pada sesi kali ini
     </p>   
+  </div>
+  <div class="container">
+    <p>{{ counter.count }}</p>
+    <p>{{ message }}</p>
+    <p>{{ message.split('').reverse().join('') }}</p>
   </div>
 </template>
 
