9ac58b0e085e9b070450808aead0cbf831aed970
ranggo pato <patoranggo@gmail.com>
2025-11-28 17:09:33 +0700

    materi: dynamic route

---
+++ src/components/pages/User.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div>
+    <h1>User page</h1>
+    <p>ID user : {{ props.id }}</p>
+</div>
+</template>
+
+<script setup>
+    // import { useRoute } from 'vue-router';
+    // const route = useRoute()
+
+    // const id = route.params.id
+
+    const props = defineProps(['id']);
+
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/pages/Home.vue";
 import Profile from "@/components/pages/Profile.vue";
 import Setting from "@/components/pages/Setting.vue";
+import User from "@/components/pages/User.vue";
 
 const routes = [
     {path: '/', component: Home},
     {path: '/profile', component: Profile},
     {path: '/setting', component: Setting},
+    {path: '/users/:id', component: User, props:true},
 ]
 
 export const router = createRouter({
